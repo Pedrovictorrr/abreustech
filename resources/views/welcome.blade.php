642e76744b50cfa0a50cc7b79d072e6b728c3d23
“PedroVictorAbreu” <pvictor@novakio.com.br>
2023-06-15 00:01:10 -0300

    link de afiliados

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,8 @@
     <title>Ashion | Template</title>
 
     <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap")}}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap")}}"
+    <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
     rel="stylesheet">
 
     <!-- Css Styles -->
