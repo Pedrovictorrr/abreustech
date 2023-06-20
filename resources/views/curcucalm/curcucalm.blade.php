0d26735d2bf695772cd0ebc866249e3f67ed4f48
Pedro Victor Abreu <82172897+Pedrovictorrr@users.noreply.github.com>
2023-06-20 13:12:56 -0300

    Update curcucalm.blade.php

--- resources/views/curcucalm/curcucalm.blade.php
+++ resources/views/curcucalm/curcucalm.blade.php
@@ -12,6 +12,23 @@
         href="https://curcumy.net.br/feed/" />
     <link rel="alternate" type="application/rss+xml" title="Feed de comentários para Curcumy &raquo;"
         href="https://curcumy.net.br/comments/feed/" />
+<!-- Meta Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '650191076977197');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=650191076977197&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
     <script>
         window._wpemojiSettings = { "baseUrl": "https:\/\/s.w.org\/images\/core\/emoji\/14.0.0\/72x72\/", "ext": ".png", "svgUrl": "https:\/\/s.w.org\/images\/core\/emoji\/14.0.0\/svg\/", "svgExt": ".svg", "source": { "concatemoji": "https:\/\/curcumy.net.br\/wp-includes\/js\/wp-emoji-release.min.js?ver=6.2.2" } };
         /*! This file is auto-generated */
